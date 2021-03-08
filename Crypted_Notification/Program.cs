ddea503735e3175f32e2fdb6ae7e85384e5a804c
Levan A <38010166+LevanAmashukeli@users.noreply.github.com>
2021-03-08 23:47:39 +0400

    Add files via upload

---
+++ Crypted_Notification/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Crypted_Notification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many numbers do you want to enter? : ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] N = new int[n];
+            int[] K = { 1, 2, 3, 4, 5 };
+            int Count = 0;
+
+            Console.WriteLine("Enter numbers for massive: ");
+            for (int i = 0; i < N.Length; i++)
+            {
+                Console.Write("{0}) ", i + 1);
+                N[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            IEnumerable<int> both = N.Intersect(K);
+
+            foreach (int id in both)
+            {
+                Count++;
+            }
+            if (Count >= 2) { Console.WriteLine("YES"); }
+            else { Console.WriteLine("NO"); }
+        }
+    }
+}
